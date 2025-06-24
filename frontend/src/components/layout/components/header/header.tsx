2e041681941bedeac38fc6f64c279264f4d29cdb
wallace-lw <wallacelw22@gmail.com>
2025-06-23 22:01:08 -0300

    feat: add personal information

--- frontend/src/components/layout/components/header/header.tsx
+++ frontend/src/components/layout/components/header/header.tsx
@@ -6,10 +6,12 @@ export const Header = () => {
 		<header className="h-16 px-16 w-full border border-b-accent border-l-0">
 			<div className="w-full flex justify-end py-4 items-center gap-3">
 				<Avatar>
-					<AvatarImage src="https://github.com/shadcn.png" />
-					<AvatarFallback>AM</AvatarFallback>
+					<AvatarImage src="https://github.com/wallace-lw.png" />
+					<AvatarFallback>WL</AvatarFallback>
 				</Avatar>
-				<Label>Arthur Morgan</Label>
+				<Label className="font-semibold font-kanit tracking-wide">
+					Wallace Leonardo
+				</Label>
 			</div>
 		</header>
 	);
