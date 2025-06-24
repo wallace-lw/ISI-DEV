3a584a831b74cedc6fd7b0e1801e3aaa9bd33466
wallace-lw <wallacelw22@gmail.com>
2025-06-23 21:42:51 -0300

    style: lint

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,4 +1,4 @@
-import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyError, FastifyReply, FastifyRequest } from "fastify";
 
 export enum AppErrorCode {
 	VALIDATION = "VALIDATION",
