217da9422367ea1c73bb3e564698dcde0c19e06e
wallace-lw <wallacelw22@gmail.com>
2025-06-25 20:04:41 -0300

    fix: import

--- frontend/src/pages/products/components/coupon-dialog/components/percentage-form/percentage-form.tsx
+++ frontend/src/pages/products/components/coupon-dialog/components/percentage-form/percentage-form.tsx
@@ -7,8 +7,7 @@ import { Button } from "@/components/ui/button";
 import { DialogClose } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useCouponsList } from "@/hooks";
-import { useApplyPercentDiscount } from "@/hooks/mutations/products/apply-percent-discount";
+import { useApplyPercentDiscount, useCouponsList } from "@/hooks";
 import { percentualSchema } from "./schema";
 import type * as T from "./types";
 
