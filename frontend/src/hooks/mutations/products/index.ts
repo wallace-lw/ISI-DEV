f2cb591fcead0a484afe4cf3d1a35fa9c7e1a987
wallace-lw <wallacelw22@gmail.com>
2025-06-25 17:00:17 -0300

    chore: barrel pattern

--- frontend/src/hooks/mutations/products/index.ts
+++ frontend/src/hooks/mutations/products/index.ts
@@ -1,2 +1,4 @@
+export * from "./apply-product-discount";
 export * from "./create-products";
+export * from "./delete-product";
 export * from "./update-product";
--- frontend/src/pages/products/components/index.ts
+++ frontend/src/pages/products/components/index.ts
@@ -1,2 +1,6 @@
+export * from "./action-buttons";
+export * from "./coupon-dialog";
+export * from "./delete-product-dialog";
 export * from "./filters";
 export * from "./products-list";
+export * from "./products-pagination";
